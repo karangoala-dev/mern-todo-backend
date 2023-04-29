104d0f4d428810fcc0f5c26fa1b229aff62d0b33
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2023-04-29 19:20:55 +0530

    Update server.js
    changed header values

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://mern-todo-app-karan.netlify.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
